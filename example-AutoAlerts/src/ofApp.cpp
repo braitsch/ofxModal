f1402683ea6b9be6f84435dac6e5521dd286624e
braitsch <stephen@braitsch.io>
2016-02-13 16:56:22 -0800

    ofxParagraph updates

--- example-AutoAlerts/src/ofApp.cpp
+++ example-AutoAlerts/src/ofApp.cpp
@@ -6,12 +6,12 @@ void ofApp::setup()
     
 // add a global alert //
     mAlert = make_shared<ofxModalAlert>();
-    mAlert->setMessageAlignment(ofxParagraph::Alignment::CENTER);
+    mAlert->setMessageAlignment(ofxParagraph::ALIGN_CENTER);
 
 // setup a simple confirm window //
     mConfirm.addListener(this, &ofApp::onModalEvent);
     mConfirm.setMessage("Are you sure you really want to do this?");
-    mConfirm.setMessageAlignment(ofxParagraph::Alignment::CENTER);
+    mConfirm.setMessageAlignment(ofxParagraph::Alignment::ALIGN_CENTER);
 // add assign it our global alert //
     mConfirm.setAlert(mAlert);
     mConfirm.show();
